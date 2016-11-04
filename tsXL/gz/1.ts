1b641fe8427c029d346df8b42897599ea735dc73
yzn <66845701@qq.com>
2016-11-04 17:50:57 +0800

    hhh

--- tsXL/gz/1.ts
+++ tsXL/gz/1.ts
@@ -1,19 +1,31 @@
-﻿var hjs: Historyjs = <any>History;
-var x = ['', {dd:5}, 4];
-_.filter(x, function (e, k) {
+﻿
+
+$('#fileupload').fileupload(<$FileUpload.IOptions>{
+    formData: {path:'images'},
+    dataType: 'json',
+    singleFileUploads: false,
+    //maxChunkSize:4096,
+    done: function (e, data) {
+        $(e.target).closest('.input-group-btn').prev().val($.map(data.result.files, function (e) {
+            return e.name;
+        }).join(","));
+       // $(this).prop('disabled',false);
+        $(this).prev().text("上传(" + data.result.files.length + ")");
+        $.each(data.result.files, function (i, file) {
+            console.log(e);
+           
+            console.log(e.relatedTarget);
+            console.log($(this));
+            
+            $('<p/>').text(file.name).appendTo(document.body);
+            console.log(file.thumbnailUrl);
+
+        });
+    },
+    progressall: function (e, data) {
+        //$('#fileupload').fileupload('disable');
+        //$(this).prop('disabled', false);
+        $(this).prev().text('正在上传');
+    }
 
 });
-
-function tests() {
-
-    if (!hjs.enabled) return false;
-    hjs.Adapter.bind(window, 'staechange', () => {
-        var state = hjs.getState();
-        hjs.log(state.data, state.title, state.url);
-
-    });
-
-    
-    hjs.pushState({ s: 1 }, '', "?state=1");
-
-}
